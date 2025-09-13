cd0cb69b0145b603185dfc096911f622dac037b8
Alex <Alexjaythompson@hotmail.com>
2025-09-13 17:47:15 +0100

    added game info view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang='ts'>
-import GameBoard from './components/GameBoard.vue';
 import generateShips from './utils/ship.ts';
+import GameBoard from './components/GameBoard.vue';
+import GameInfo from "./components/GameInfo.vue";
 
 const ships = generateShips();
 </script>
 
 <template>
+  <GameInfo
+    :moves="0"
+    :ships-remaining="3"
+    :last-hit="undefined"
+  />
   <GameBoard
     :rows='9'
     :cols='9'
---
+++ src/components/GameInfo.vue
@@ -0,0 +1,31 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  moves?: number;
+  shipsRemaining?: number;
+  lastHit?: boolean;
+}>();
+</script>
+
+<template>
+  <div class='game-info'>
+    <div>Moves: {{ props.moves }}</div>
+    <div>{{ props.shipsRemaining }} ships remaining</div>
+    <div>
+      <span v-if="props.lastHit !== undefined">
+        {{ props.lastHit ? 'Hit!' : 'Miss!' }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.game-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  border: 1px solid #444;
+  border-radius: 12px;
+  height: 3rem;
+}
+</style>
